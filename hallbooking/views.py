c38f43f62a9837a2ca4bc1620192d299667c9623
Ajay Prasadh <ajay@ajayprasadh.com>
2014-09-11 03:15:31 +0530

    check feature done

--- hallbooking/views.py
+++ hallbooking/views.py
@@ -1,10 +1,8 @@
-# Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response,redirect
 from hallbooking.models import Room,Bookings
 from hallbooking.forms import TestForm
-
 def home(request):
 	context = RequestContext(request)
 	form=TestForm()
@@ -18,9 +16,20 @@ def home(request):
 			date_to_check=str(form['date_booking'].value())
 			bookings_that_day=Bookings.objects.filter(date_booking=date_to_check)
 			status={}
+			y=''
 			for books in bookings_that_day:
-				status[str(books)]=False
-			context_dict={'Rooms':room_list,'form':form,'x':status}
+				b_end=int(books.endtime)
+				b_start=int(books.starttime)
+				f_start=int(form['starttime'].value())
+				f_end=int(form['endtime'].value())
+				if(b_end<=f_start and b_end<=f_end):
+					y='safe machan'
+				elif(b_start>=f_start and b_start>=f_end):
+					y='you are also fine'
+				else:	
+					status[str(books)]=False
+					y=''
+			context_dict={'Rooms':room_list,'form':form,'x':status,'y':y}
 			return render_to_response('hallbooking/home.html',context_dict,context)			
 		else:
 			print form.errors
@@ -37,3 +46,7 @@ def home(request):
 def index(request):
 	context = RequestContext(request)
 	return render_to_response('hallbooking/index.html', context)
+def check_availability(request):
+	context = RequestContext(request)
+	blah=request.GET['data']
+	return HttpResponse(blah)
